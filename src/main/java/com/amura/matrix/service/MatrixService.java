959c7eface52c2ced725db30c6033601db1f73af
sayal <sayal@DESKTOP-MDO29B8>
2019-03-30 01:38:05 +0530

    Added pom

--- src/main/java/com/amura/matrix/service/MatrixService.java
+++ src/main/java/com/amura/matrix/service/MatrixService.java
@@ -3,14 +3,13 @@ package com.amura.matrix.service;
 import com.amura.matrix.model.Submatrix;
 
 /**
- * Matrix service interface indicating services
- *  provided to play with matrix
+ * Matrix service interface indicating services provided to play with matrix
  * 
  * @author Sayali Doshi
  * @since 29-03-2019
  */
 public interface MatrixService {
-	
-	public Submatrix findLongestSubMatrix(Integer[][] matrix );
+
+	public Submatrix findLongestSubMatrix(Integer[][] matrix);
 
 }
